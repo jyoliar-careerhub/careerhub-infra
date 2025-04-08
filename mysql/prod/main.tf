3fc9c6ce9f226f57391ea3bccef648834359b063
jyo liar <jyo-liar@gmail.com>
2025-04-08 20:04:44 +0900

    mysql 이름 앞에 env prefix 추가

--- mysql/prod/main.tf
+++ mysql/prod/main.tf
@@ -8,7 +8,7 @@ module "mysql" {
   for_each = local.mysqls
 
   source = "../_modules/aurora"
-  name   = each.key
+  name   = "${var.env}-${each.key}"
 
   vpc_id     = local.careerhub_subnets_outputs.vpc_id
   subnet_ids = local.careerhub_subnets_outputs.private_subnet_ids
