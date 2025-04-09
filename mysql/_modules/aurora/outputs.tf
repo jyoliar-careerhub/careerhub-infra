5375f4cf4758b67225164a59b878a1d94febefa5
jyo liar <jyo-liar@gmail.com>
2025-04-09 13:44:30 +0900

    코드 오류 수정

--- mysql/_modules/aurora/outputs.tf
+++ mysql/_modules/aurora/outputs.tf
@@ -12,7 +12,7 @@ output "user_secret_arn" {
   value = aws_rds_cluster.this.master_user_secret[0].secret_arn
 }
 
-output "mysql_security_group_id" {
+output "security_group_id" {
   description = "The security group ID of the Aurora cluster."
   value       = aws_security_group.this.id
 }
--- mysql/prod/main.tf
+++ mysql/prod/main.tf
@@ -15,19 +15,19 @@ module "mysql" {
 }
 
 data "aws_subnet" "this" {
-  for_each = concat(local.careerhub_subnets_outputs.private_subnet_ids, local.careerhub_subnets_outputs.public_subnet_ids)
+  for_each = toset(concat(local.careerhub_subnets_outputs.private_subnet_ids, local.careerhub_subnets_outputs.public_subnet_ids))
 
   id = each.value
 }
 
 resource "aws_security_group_rule" "allow_mysql" {
-  for_each = data.aws_subnet.this
+  for_each = module.mysql
 
   type              = "ingress"
   from_port         = 3306
   to_port           = 3306
   protocol          = "tcp"
-  cidr_blocks       = [each.value.cidr_block]
-  ipv6_cidr_blocks  = each.value.ipv6_cidr_block != null ? [each.value.ipv6_cidr_block] : []
-  security_group_id = "sg-123456"
+  cidr_blocks       = [for subnet in data.aws_subnet.this : subnet.cidr_block]
+  ipv6_cidr_blocks  = [for subnet in data.aws_subnet.this : subnet.ipv6_cidr_block if subnet.ipv6_cidr_block != null && subnet.ipv6_cidr_block != ""]
+  security_group_id = each.value.security_group_id
 }
