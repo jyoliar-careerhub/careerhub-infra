cc34999ea775e36858e2d5ef2225db4bb6e2b2fb
jyo liar <jyo-liar@gmail.com>
2025-04-09 12:23:45 +0900

    mysql security group rule 추가

--- mysql/prod/main.tf
+++ mysql/prod/main.tf
@@ -13,3 +13,21 @@ module "mysql" {
   vpc_id     = local.careerhub_subnets_outputs.vpc_id
   subnet_ids = local.careerhub_subnets_outputs.private_subnet_ids
 }
+
+data "aws_subnet" "this" {
+  for_each = concat(local.careerhub_subnets_outputs.private_subnet_ids, local.careerhub_subnets_outputs.public_subnet_ids)
+
+  id = each.value
+}
+
+resource "aws_security_group_rule" "allow_mysql" {
+  for_each = data.aws_subnet.this
+
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = [each.value.cidr_block]
+  ipv6_cidr_blocks  = each.value.ipv6_cidr_block != null ? [each.value.ipv6_cidr_block] : []
+  security_group_id = "sg-123456"
+}
