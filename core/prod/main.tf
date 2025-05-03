8bed02afeb29f55d151f10f66b57d29931c35d24
jyo liar <jyo-liar@gmail.com>
2025-05-03 16:37:10 +0900

    codepipeline: 잘못된 ecr domain parsing 이슈 처리

--- core/prod/main.tf
+++ core/prod/main.tf
@@ -1,13 +1,38 @@
 locals {
   code_pipelines = {
     posting-provider = {
-      source = "./backend_cicd_infra"
-
-      cicd_name  = "posting-provider"
-      build_arch = "arm64"
-
+      cicd_name       = "posting-provider"
       repository_path = "jyoliar-careerhub/careerhub-posting-provider"
     }
+    # posting-service = {
+    #   cicd_name       = "posting-service"
+    #   repository_path = "jyoliar-careerhub/careerhub-posting-service"
+    # }
+
+    # posting-skillscanner = {
+    #   cicd_name       = "posting-skillscanner"
+    #   repository_path = "jyoliar-careerhub/careerhub-posting-skillscanner"
+    # }
+    # review-crawler = {
+    #   cicd_name       = "review-crawler"
+    #   repository_path = "jyoliar-careerhub/careerhub-review-crawler"
+    # }
+    # review-service = {
+    #   cicd_name       = "review-service"
+    #   repository_path = "jyoliar-careerhub/careerhub-review-service"
+    # }
+    # userinfo-service = {
+    #   cicd_name       = "userinfo-service"
+    #   repository_path = "jyoliar-careerhub/careerhub-userinfo-service"
+    # }
+    # api-composer = {
+    #   cicd_name       = "api-composer"
+    #   repository_path = "jyoliar-careerhub/careerhub-api-composer"
+    # }
+    # auth-service = {
+    #   cicd_name       = "auth-service"
+    #   repository_path = "jyoliar-careerhub/auth-service"
+    # }
   }
 }
 
@@ -28,7 +53,7 @@ module "code_pipeline" {
   for_each = local.code_pipelines
 
   name            = "${var.env}-${each.value.cicd_name}"
-  build_arch      = each.value.build_arch
+  build_arch      = "arm64"
   repository_path = each.value.repository_path
   branch_name     = "prod"
 
